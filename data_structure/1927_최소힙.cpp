94f6f1a2882708295e5fe959a85cd8e4ccd8e301
ssinee <skvkffish@gmail.com>
2022-01-25 12:58:51 +0900

    solve: data_structure-1927

---
+++ data_structure/1927_최소힙.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    priority_queue<long> pq;
+    int N;
+    cin >> N;
+
+    while (N--) {
+        long x;
+        cin >> x;
+        if (x == 0) {
+            if (pq.empty()) {
+                cout << 0 << "\n";
+                continue;
+            }
+            cout << -pq.top() << "\n";
+            pq.pop();
+        } else {
+            pq.push(-x);
+        }
+    }
+    return 0;
+}
